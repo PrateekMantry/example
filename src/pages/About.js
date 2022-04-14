58eb54ffed49991c453b4408d2c38a5e2372838b
Prateek Mantry <1905695@kiit.ac.in>
2022-04-14 18:33:25 +0530

    about page is changed

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,6 +2,7 @@ const About = () =>{
     return(
         <div className="alert alert-primary" role="alert">
           <h1>About page!</h1>
+          <p>this my about page</p>
         </div>
     );
 }
